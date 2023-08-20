3d68a2be211e93a60bd612ed0c62c4be213c75c4
Ilya Siganov <ilya.blan4@gmail.com>
2023-08-20 19:39:57 +0600

    new day - new version of the chat

--- web/main.py
+++ web/main.py
@@ -1,76 +1,182 @@
+import os
+import shutil
 from typing import Dict
-from fastapi import Cookie, FastAPI, Request, Response, WebSocket, WebSocketDisconnect
+from fastapi import (
+    Cookie,
+    FastAPI,
+    Form,
+    Request,
+    Response,
+    UploadFile,
+    WebSocket,
+    WebSocketDisconnect,
+    status,
+)
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from uuid import uuid4, UUID
+from uuid import uuid4
 import time
+from pathlib import Path
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+
 
 def now_ts():
     return int(time.time() * 1000)
 
+
+class UserModel(BaseModel):
+    name: str
+    password: str
+
+
 class ConnectionManager:
     def __init__(self) -> None:
-        self.connections: Dict[UUID, WebSocket] = {}
+        self.connections: Dict[WebSocket, UserModel] = {}
 
     @property
     def users(self):
-        return [str(uid) for uid in self.connections.keys()]
+        return list({user.name for user in self.connections.values()})
 
-    async def connect(self, ws: WebSocket):
+    async def connect(self, ws: WebSocket, user: UserModel):
         await ws.accept()
-        uid = uuid4()
-        self.connections[uid] = ws
-        return uid
-    
-    def disconnect(self, uid: UUID):
-        self.connections.pop(uid)
+        self.connections[ws] = user
 
-    async def broadcast(self, msg):
-        msg['ts'] = now_ts()
-        for uid, ws in self.connections.items():
+    def disconnect(self, ws: WebSocket):
+        self.connections.pop(ws)
+
+    async def broadcast(self, msg: Dict[str, any]):
+        msg["ts"] = now_ts()
+        msg["mid"] = str(uuid4())
+        for ws in self.connections:
             await ws.send_json(msg)
 
 
+MEDIA_DIR = Path("media")
+
 app = FastAPI()
-app.mount('/static', StaticFiles(directory='static'), name='static')
-templates = Jinja2Templates('templates')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/media", StaticFiles(directory=MEDIA_DIR), name="media")
+templates = Jinja2Templates("templates")
 cm = ConnectionManager()
 
-CHAT_BOT = 'ChatBot'
+users: Dict[str, UserModel] = {}
 
-@app.websocket('/ws')
-async def ws_endpoint(ws: WebSocket):
-    uid = await cm.connect(ws)
+CHAT_BOT = "ChatBot"
+
+
+@app.websocket("/ws")
+async def ws_endpoint(
+    ws: WebSocket,
+    username: str | None = Cookie(default=None),
+):
+    print(f"WS username={username}")
+    user = users.get(username)
+    if user is None:
+        return
+
+    await cm.connect(ws, user)
     try:
-        await cm.broadcast({
-            'from': CHAT_BOT,
-            'type': 'users_list',
-            'users': cm.users,
-            'text': f'Client connected {uid}',
-        })
+        await cm.broadcast(
+            {
+                "from": CHAT_BOT,
+                "type": "users_list",
+                "users": cm.users,
+                "text": f"Client connected {username}",
+            }
+        )
         while True:
             txt = await ws.receive_text()
-            print('>', txt)
-            message_id = uuid4()
+            if len(txt) > 512:
+                continue
+            print(f"{username}> ", txt)
             await cm.broadcast({
-                'from': str(uid),
-                'text': txt,
-                'mid': str(message_id),
+                "from": username,
+                "text": txt,
             })
     except WebSocketDisconnect:
-        cm.disconnect(uid)
-        print(f'Client disconnected {uid}')
-        await cm.broadcast({
-            'from': CHAT_BOT,
-            'type': 'users_list',
-            'users': cm.users,
-            'text': f'Client disconnected {uid}',
-        })
+        cm.disconnect(ws)
+        print(f"Client disconnected {username}")
+        await cm.broadcast(
+            {
+                "from": CHAT_BOT,
+                "type": "users_list",
+                "users": cm.users,
+                "text": f"Client disconnected {username}",
+            }
+        )
 
-@app.get('/')
-def index(request: Request):
+
+@app.get("/")
+def index(
+    request: Request,
+    username: str | None = Cookie(default=None),
+):
+    print(f"GET / username={username}")
+    user = users.get(username)
+    if user is None:
+        return templates.TemplateResponse(
+            "login.html",
+            {"request": request},
+        )
     return templates.TemplateResponse(
-        'index.html', {'request': request},
+        "index.html",
+        {"request": request},
     )
+
+
+@app.post("/login")
+def login(
+    request: Request,
+    username: str = Form(),
+    password: str = Form(),
+):
+    username = username.strip()
+    password = password.strip()
+    if len(username) == 0 or len(password) == 0:
+        return templates.TemplateResponse("login.html", {"request": request})
+    user = users.get(username)
+    if user is not None and user.password != password:
+        # TODO: пароль или имя неправильные
+        return templates.TemplateResponse("login.html", {"request": request})
+
+    # либо он новый либо старый и корректный юзер
+    if user is None:
+        user = UserModel(
+            name=username,
+            password=password,
+        )
+        users[username] = user
+
+    response = RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie("username", username)
+    return response
+
+
+@app.post("/upload")
+async def create_upload_files(
+    file: UploadFile,
+    username: str | None = Cookie(default=None),
+):
+    user = users.get(username)
+    if user is None:
+        return Response(status_code=status.HTTP_403_FORBIDDEN)
+
+    fid = str(uuid4())
+    if file.filename is not None:
+        parts = file.filename.split(os.extsep)
+        if len(parts) > 1:
+            ext = parts[-1]
+            fid += f".{ext}"
+    print(f"username={username} fid={fid}")
+
+    new_file = MEDIA_DIR / fid
+    with new_file.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    await cm.broadcast({
+        "from": username,
+        "image": f"/media/{fid}",
+    })
+    return {"filename": f"/media/{fid}"}
--- web/static/main.js
+++ web/static/main.js
@@ -16,15 +16,39 @@ ws.onmessage = (event) => {
   const data = JSON.parse(event.data);
   console.log(data);
   const li = document.createElement('li');
-  li.textContent = `FROM: ${data.from} > ${data.text}`;
+  if (data.text) {
+    li.textContent = `FROM: ${data.from} > ${data.text}`;
+  }
+  if (data.image) {
+    const img = document.createElement('img');
+    img.src = data.image;
+    img.style = "width: 400px";
+    const p = document.createElement('p');
+    p.textContent =  `FROM: ${data.from}`;
+    li.appendChild(p);
+    li.appendChild(img);
+  }
   messages.appendChild(li);
 }
 
 function send(event) {
+  sendFile();
   const txt = input.value.trim();
   if (txt.length > 0) {
     ws.send(input.value);
   }  
   input.value = "";
   event.preventDefault();
+}
+
+function sendFile() {
+  const fileInput = document.getElementById('uploader'); // получаем элемент input для загрузки файла 
+  const file = fileInput.files[0]; // получаем выбранный файл 
+  if (!file) return;
+  const xhr = new XMLHttpRequest(); // создаем объект XMLHttpRequest 
+  const formData = new FormData(); // создаем объект FormData для передачи файла 
+  formData.append('file', file); // добавляем файл в объект FormData 
+  xhr.open('POST', '/upload'); // указываем метод и URL сервера, куда будет отправлен файл 
+  xhr.send(formData); // отправляем запрос на сервер с помощью метода send()
+  fileInput.value = '';
 }
